5c7f8548763608f7b8a0c3c5f3ab3b0a49407d04
sonjaolson <sonjaolson@newrelic.com>
2020-09-24 17:00:54 -0700

    Move out to Readme

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,16 +9,6 @@ function Home() {
         <img src={logo} className="home-logo" alt="logo" />
         <p>Get Started with React Pack</p>
       </header>
-      <div className="container">
-        <div className="container-inner">
-          <p>Libraries to add: </p>
-          <p>Styling?</p>
-          <li>Styled Components</li>
-          <li>Sass</li>
-          <p>TypeScript?</p>
-          <li>Getting started with typescript</li>
-        </div>
-      </div>
     </div>
   );
 }
